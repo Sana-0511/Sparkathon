ded4fca7f4086832858bb35e3668eb56f2fcad24
Dhritisha03 <1032211764@mitwpu.edu.in>
2023-08-12 16:21:15 +0530

    Dhriti

---
+++ my-app/src/Component/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navbar.css';
+// import {  Link } from "react-router-dom";
+const Navbar= () =>{
+  return (
+  <div className="navbar">
+    <div className="rectangle">
+        <img src="https://logos-download.com/wp-content/uploads/2016/02/Walmart_logo_transparent_png_blue.png"></img></div>
+  </div>
+  );
+}
+export default Navbar;
