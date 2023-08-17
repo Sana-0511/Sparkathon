f473856d52a11337b1a4b7e3ea4472796a04d44c
gauriket <gauriket@gmail.com>
2023-08-18 00:18:09 +0530

    No Logo

--- my-app/src/Component/Navbar.js
+++ my-app/src/Component/Navbar.js
@@ -5,7 +5,7 @@ const Navbar= () =>{
   return (
   <div className="navbar">
     <div className="rectangle">
-        <img src="https://logos-download.com/wp-content/uploads/2016/02/Walmart_logo_transparent_png_blue.png"></img></div>
+       <div className="logo">Walmart</div></div>
   </div>
   );
 }
